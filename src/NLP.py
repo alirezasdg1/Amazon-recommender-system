b81bf893833ca804b5b8090677c218d1e7d9e26b
Ubuntu <ubuntu@ip-172-31-70-68.ec2.internal>
2020-09-16 16:57:59 +0000

    C-based

--- src/NLP.py
+++ src/NLP.py
@@ -6,7 +6,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 def nlp_process(df,max_features=None):
 
-    vectorizer = TfidfVectorizer(min_df=0.005, stop_words='english', max_features = max_features)
+    vectorizer = TfidfVectorizer(min_df=0.001, stop_words='english', max_features = 10000)
     m_nlp = vectorizer.fit_transform(df).toarray()
     df_nlp = pd.DataFrame(m_nlp, columns=vectorizer.get_feature_names())
     
