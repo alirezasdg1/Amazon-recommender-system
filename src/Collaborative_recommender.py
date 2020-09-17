018720bcb40465e22af8d32b4ab32c22b3c44e37
Ubuntu <ubuntu@ip-172-31-51-31.ec2.internal>
2020-09-17 21:33:39 +0000

    v5

---
+++ src/Collaborative_recommender.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import boto3
+import pandas as pd
+import numpy as np
+import sys
+from io import StringIO
+# import pyspark
+# from pyspark.sql import SparkSession
+
+if __name__ == "__main__":
+    # spark = SparkSession.builder.getOrCreate()
+    pass
