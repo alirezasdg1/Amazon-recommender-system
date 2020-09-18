bde92a037cb72dd577cafbee40cbac8a0b4a9979
Ubuntu <ubuntu@ip-172-31-53-166.ec2.internal>
2020-09-18 15:55:45 +0000

    v5

--- src/Collaborative_recommender.py
+++ src/Collaborative_recommender.py
@@ -1,12 +1,54 @@
-import numpy as np
-import pandas as pd
-import boto3
 import pandas as pd
 import numpy as np
 import sys
 from io import StringIO
-# import pyspark
-# from pyspark.sql import SparkSession
+import surprise
+from sklearn.model_selection import train_test_split, GridSearchCV
+
+import random
+
+from surprise import SVD
+from surprise import Dataset
+from surprise import accuracy
+from surprise.model_selection import GridSearchCV
+
+class CollaborativeRecommender():
+    
+    def __init__(self,df,reader):
+        self.data = surprise.Dataset.load_from_df(df,reader)
+        self.trainset = self.data.build_full_trainset()
+        
+        self.alog = None
+        
+    
+    
+    def grid(self,param_grid,cv=3):
+        raw_ratings = self.data.raw_ratings
+        random.shuffle(raw_ratings)
+        # A = 90% of the data, B = 10% of the data
+        threshold = int(.9 * len(raw_ratings))
+        A_raw_ratings = raw_ratings[:threshold]
+        B_raw_ratings = raw_ratings[threshold:]
+        self.data.raw_ratings = A_raw_ratings  # data is now the set A
+
+        grid_search = GridSearchCV(SVD, param_grid, measures=['rmse'], cv=3)
+        grid_search.fit(self.data)
+        self.algo = grid_search.best_estimator['rmse']
+
+        return self
+
+    def fit(self):
+        
+        self.algo.fit(self.trainset)
+        return self
+
+    def predict(self):
+        predictions = self.algo.test(self.trainset.build_testset())
+        
+        return accuracy.rmse(predictions)
+
+
+
 
 if __name__ == "__main__":
     # spark = SparkSession.builder.getOrCreate()
