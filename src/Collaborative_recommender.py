f53c981098c1aa0f3a9b762d647410825f8e88cc
Ubuntu <ubuntu@ip-172-31-46-232.ec2.internal>
2020-09-19 21:40:36 +0000

    col recom.

--- src/Collaborative_recommender.py
+++ src/Collaborative_recommender.py
@@ -7,17 +7,17 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 
 import random
 
-from surprise import SVD
+from surprise import SVD, NMF
 from surprise import Dataset
 from surprise import accuracy
 from surprise.model_selection import GridSearchCV
 
 class CollaborativeRecommender():
     
-    def __init__(self,df,reader):
+    def __init__(self,df,reader,model):
         self.data = surprise.Dataset.load_from_df(df,reader)
         self.trainset = self.data.build_full_trainset()
-        
+        self.model = model
         self.alog = None
         
     
@@ -31,7 +31,7 @@ class CollaborativeRecommender():
         B_raw_ratings = raw_ratings[threshold:]
         self.data.raw_ratings = A_raw_ratings  # data is now the set A
 
-        grid_search = GridSearchCV(SVD, param_grid, measures=['rmse'], cv=3)
+        grid_search = GridSearchCV(NMF, param_grid, measures=['rmse'], cv=3)
         grid_search.fit(self.data)
         self.algo = grid_search.best_estimator['rmse']
 
@@ -42,7 +42,11 @@ class CollaborativeRecommender():
         self.algo.fit(self.trainset)
         return self
 
-    def predict(self):
+    def similarity(self):
+        sim_options = {'name': 'cosine', 'user_based': False}
+        return self.algo.compute_similarities(sim_options)    
+
+    def accuracy(self):
         predictions = self.algo.test(self.trainset.build_testset())
         
         return accuracy.rmse(predictions)
