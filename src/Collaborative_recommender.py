f2074f0583b894c58169e2b597113d432e0b45ee
Ubuntu <ubuntu@ip-172-31-46-232.ec2.internal>
2020-09-19 22:24:17 +0000

    col recom

--- src/Collaborative_recommender.py
+++ src/Collaborative_recommender.py
@@ -46,10 +46,61 @@ class CollaborativeRecommender():
         sim_options = {'name': 'cosine', 'user_based': False}
         return self.algo.compute_similarities(sim_options)    
 
-    def accuracy(self):
-        predictions = self.algo.test(self.trainset.build_testset())
+    def predictions(self):
+        self.predictions = self.algo.test(self.trainset.build_testset())
+        acc = accuracy.rmse(self.predictions)
         
-        return accuracy.rmse(predictions)
+        return self.predictions, acc
+    
+    def get_Iu(self,uid):
+        """ return the number of items rated by given user
+        args: 
+        uid: the id of the user
+        returns: 
+        the number of items rated by the user
+        """
+        try:
+            return len(self.trainset.ur[self.trainset.to_inner_uid(uid)])
+        except ValueError: # user was not part of the trainset
+            return 0
+    
+    def get_Ui(self,iid):
+        """ return number of users that have rated given item
+        args:
+        iid: the raw id of the item
+        returns:
+        the number of users that have rated the item.
+        """
+        try: 
+            return len(self.trainset.ir[self.trainset.to_inner_iid(iid)])
+        except ValueError:
+            return 0
+    
+    # def get_top_n(predictions, n=10):
+    # """Return the top-N recommendation for each user from a set of predictions.
+
+    # Args:
+    #     predictions(list of Prediction objects): The list of predictions, as
+    #         returned by the test method of an algorithm.
+    #     n(int): The number of recommendation to output for each user. Default
+    #         is 10.
+
+    # Returns:
+    # A dict where keys are user (raw) ids and values are lists of tuples:
+    #     [(raw item id, rating estimation), ...] of size n.
+    # """
+
+    # # First map the predictions to each user.
+    # top_n = defaultdict(list)
+    # for uid, iid, true_r, est, _ in predictions:
+    #     top_n[uid].append((iid, est))
+
+    # # Then sort the predictions for each user and retrieve the k highest ones.
+    # for uid, user_ratings in top_n.items():
+    #     user_ratings.sort(key=lambda x: x[1], reverse=True)
+    #     top_n[uid] = user_ratings[:n]
+
+    # return top_n
 
 
 
